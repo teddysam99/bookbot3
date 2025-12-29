67e961bce791fb846529c7f257aaafe69b799577
teddysam99 <teddysamyuri19@gmail.com>
2025-12-29 09:44:39 -0500

    3. Even though my understanding of python has massively increase there are tons of things that I still need to learn. Onwards to funtional programming.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 from stats import count_word
 from stats import count_char
+from stats import dict_sorted_list
 
 
 
@@ -10,13 +12,21 @@ def get_book_text(path_to_file):
     
 
 def main():
-    text = "books/frankenstein.txt"
-    word_count = count_word(get_book_text(text))
-    dict_char_count = count_char(get_book_text(text))
-    #print(get_book_text(text))
-    #print(word_count)
-    print(dict_char_count)
-
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 {sys.argv[0]} <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    word_count = count_word(get_book_text(path))
+    dict_char_count = dict_sorted_list(count_char(get_book_text(path)))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in dict_char_count:
+        ch = item["char"]
+        print(f"{ch}: {item['num']}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,13 +1,25 @@
 def count_char(text):
-    dict_of_letters = {}
-    lower_case = text.lower()
-    words = lower_case.split
-    for word in lower_case:
-        if word not in dict_of_letters:
-            dict_of_letters[word] = 1
-        elif word in dict_of_letters:
-            dict_of_letters[word] += 1
-    return dict_of_letters
+    char_dict = {}
+    words = text.split()
+    for word in words:
+        lower_case = word.lower()
+        for letter in lower_case:
+            if letter not in char_dict:
+                char_dict[letter] = 1
+            else:
+                char_dict[letter] += 1
+    return char_dict
+ 
 
 def count_word(text):
-    print(f"total {len(text.split())} number of words")
+    return len(text.split())
+
+def sort_on(items):
+    return items["num"]
+
+def dict_sorted_list(char_dict):
+    char_list = []
+    for char, num in char_dict.items():
+        char_list.append({"char": char, "num": num})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
